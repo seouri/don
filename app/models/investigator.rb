580680f9fbde3b924697940d89dcca8d7f41af9a
Joon Lee <joon@leelaboratory.com>
2008-07-14 16:00:54 -0400

    added awarded years for investigator

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -1,4 +1,9 @@
 class Investigator < ActiveRecord::Base
   has_many :grants, :order => "grants.year desc"
   has_many :organizations, :through => :grants
+  
+  def years
+    grants.collect(&:year).uniq.sort
+  end
+  
 end
