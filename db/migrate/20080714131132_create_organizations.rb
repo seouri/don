5c751fad08e85718548ff7a3ea467c0232e1c000
Joon Lee <joon@leelaboratory.com>
2008-07-15 16:58:11 -0400

    added award_total column to organizations & investigators

--- db/migrate/20080714131132_create_organizations.rb
+++ db/migrate/20080714131132_create_organizations.rb
@@ -4,10 +4,12 @@ class CreateOrganizations < ActiveRecord::Migration
       t.string :name
       t.string :city
       t.string :state
+      t.integer :award_total, :limit => 8
     end
     add_index :organizations, :name
     add_index :organizations, :city
     add_index :organizations, :state
+    add_index :organizations, :award_total
   end
 
   def self.down
--- db/migrate/20080714141500_create_investigators.rb
+++ db/migrate/20080714141500_create_investigators.rb
@@ -2,8 +2,10 @@ class CreateInvestigators < ActiveRecord::Migration
   def self.up
     create_table :investigators do |t|
       t.string :name
+      t.integer :award_total
     end
     add_index :investigators, :name
+    add_index :investigators, :award_total
   end
 
   def self.down
