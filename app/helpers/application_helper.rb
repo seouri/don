b6bdd19156036adfece2de12c50afa197d3e3afd
Joon Lee <joon@leelaboratory.com>
2008-07-15 14:34:54 -0400

    finished smar_year

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,26 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def smart_year(years)
-    years.join(", ")
+    if years.size < 3
+      return years.join(", ")
+    else
+      consecutive = []
+      years.each do |year|
+        index = years.index(year)
+        if index == 0
+          consecutive.push(year)
+        elsif index == years.size - 1
+          consecutive.push(year)
+        elsif year == years[index - 1] + 1 and year == years[index + 1] - 1
+          consecutive.push("-") unless consecutive.last == "-"
+        elsif year == years[index - 1] + 1 and year != years[index + 1] - 1
+          consecutive.push(year)
+          consecutive.push(", ")
+        elsif year != years[index - 1] + 1 and year == years[index + 1] - 1
+          consecutive.push(year)
+        end  
+      end
+      consecutive.join("")
+    end
   end
 end
