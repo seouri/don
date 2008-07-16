1b84a5eb758769d22892e60ff16db441264f8f1c
Joon Lee <joon@leelaboratory.com>
2008-07-16 10:07:33 -0400

    added grant_type

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -1,4 +1,10 @@
 class Grant < ActiveRecord::Base
   belongs_to :organization
   belongs_to :investigator
+  
+  named_scope :recent, :order => "grants.year desc, grants.award desc", :limit => 10, :include => :investigator
+  
+  def grant_type
+    grant_number.gsub(/^\d/, '').slice(0, 3)
+  end
 end
