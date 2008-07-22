e29db388503570cd324ac70446c131adc1830ce3
Joon Lee <joon@leelaboratory.com>
2008-07-22 15:52:20 -0400

    added number_to_human_currency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,4 +32,14 @@ module ApplicationHelper
     end
   end
 
+  def number_to_human_currency(number)
+    digits = Math.log10(number)
+    case
+      when digits < 3; number
+      when digits < 6; "$%.1f K" % (number / 10 ** 3)
+      when digits < 9; "$%.1f M" % (number / 10 ** 6)
+      when digits < 12; "$%.1f B" % (number / 10 ** 9)
+      else number
+    end
+  end
 end
