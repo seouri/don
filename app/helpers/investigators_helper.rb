e8fd8627a475093e07c2b23dc2203ef95a49c031
Joon Lee <joon@leelaboratory.com>
2008-07-25 11:10:43 -0400

    added organization by year chart

--- app/helpers/investigators_helper.rb
+++ app/helpers/investigators_helper.rb
@@ -42,4 +42,36 @@ module InvestigatorsHelper
     url = Gchart.pie(:data => awards, :labels => categories, :size => "#{width}x80", :custom => 'chco=999999')
     image_tag(url, :alt => "Awards by Category")
   end
+  
+  def organization_chart(grants)
+    year = grants.map {|g| g.year}.uniq.sort
+    year_min = year.min - 1
+    stat = grants.group_by {|g| g.organization.name }
+    organization = stat.keys.sort {|a,b| stat[a].map {|k| k.year}.min <=> stat[b].map {|l| l.year}.min }
+    x = []
+    y = []
+    y_value = 0
+    organization.each do |o|
+      y_value += 4
+      years = stat[o].map {|k| k.year}.uniq
+      years.each do |i|
+        award = stat[o].map {|k| k.award.to_i}.sum
+        x.push(i - year_min)
+        y.push(y_value.to_i)
+      end
+    end
+    max = [x.max, y.max].max.to_f
+    factor = x.max > y.max ? y.max.to_f / max : x.max.to_f / max
+    x.map! {|a| a.to_f * factor} if x.max > y.max
+    y.map! {|a| a.to_f * factor} if x.max < y.max
+    years = (year.first .. year.last).to_a
+    x_label = years.map {|i| years.index(i) % 2 == 1 ? " " : i }
+    x_label.unshift(" ")
+    y_label = organization
+    y_label.unshift(" ")
+    width = organization.sort {|a,b| a.length <=> b.length}.max.length * 8 + year.size * 14 
+    height = (organization.size + 1) * 10 + 14
+    url = Gchart.scatter(:data => [x, y], :size => "#{width}x#{height}", :axis_with_labels => "x,r", :axis_labels => [x_label, y_label], :custom => "chm=o,666666aa,1,1,10")
+    image_tag(url, :alt => "Organization by Year")
+  end
 end
