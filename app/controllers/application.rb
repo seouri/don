e704334d583bfc1125a600a5563a5f1bff74a13a
Joon Lee <joon@leelaboratory.com>
2008-09-22 13:57:39 -0400

    clean up query string

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,6 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  before_filter :clean_query
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -12,4 +13,7 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  def clean_query
+    params[:q].squish! unless params[:q].blank?
+  end
 end
